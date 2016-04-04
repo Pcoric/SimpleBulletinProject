fe6a8ca8344d6b0e2fd0515413fa08a2a4e4de57
Pavle Coric <coric.2@osu.edu>
2016-04-04 19:38:08 -0400

    committing my files

---
+++ src/Controller.java
@@ -0,0 +1,78 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Controller {
+    public class ThreadReader extends Thread {
+        private OutThread ot;
+        private Socket Socket;
+        //boolean?
+        
+        public ThreadReader(Socket socket) {
+            this.Socket = socket;
+        }
+        public void begin() {
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(this.Socket.getInputStream()));
+                //this.ot = new OutThread();
+                Users user = new Users();
+                this.ot = new OutThread(this.Socket, user); //fix
+                this.ot.setCurrOut("various information");
+                while (true) {
+                    this.ot.setCurrOut(this.getCommand(input.readLine()));
+                }
+                //this.Socket.close();
+            } catch (Exception e) {
+                System.out.println("ERROR!");
+            }
+        }
+        public String getCommand(String command) {
+            String end = "";
+            //action requests
+            return end;
+        }
+    }
+    public class OutThread extends Thread {
+        private PrintWriter pw;
+        private Users Users;
+        private String currOut = "empty";
+        public OutThread(Socket socket, Users user) {
+            this.Users = user;
+            try {
+                this.pw = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            this.start();
+        }
+        public void begin() {
+            while (true) {
+                this.pw.print("Enter a command!");
+                this.pw.println("response: " + this.getCurrentMessage());
+                this.setCurrOut("empty");
+                this.pw.flush();
+            }
+        }
+        public String getCurrentMessage() {
+            return this.currOut;
+        }
+        public void setCurrOut(String message) {
+            this.currOut = message;
+        }
+    }
+    public class Users {
+        String ID;
+        public void setUser(String id) {
+            this.ID = id;
+        }
+        public void User() {
+        }
+        public String getUserID() {
+            return this.ID;
+        }
+    }
+}
--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,4 @@
-package webserver;
+
 
 public class Model {
     
