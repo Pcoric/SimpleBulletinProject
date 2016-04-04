91c7d1cf72879e65f5b1b4f3ae95e0e76d3ce2de
ZJG7 <zgiof@ZJG-MBP.local>
2016-04-04 17:20:11 -0400

    sup

--- src/webserver/BulletinMain.java
+++ src/webserver/BulletinMain.java
@@ -4,29 +4,29 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class BulletinMain {
-    public static void main(String args[]) throws Exception {
-        ServerSocket currSocket = new ServerSocket();
+public BulletinMain {
+public static void main(String args[]) throws Exception {
+ServerSocket currSocket = new ServerSocket();
 
-        try {
-            InetSocketAddress currAddress = new InetSocketAddress(
-                    Integer.parseInt(args[0]));
+try {
+InetSocketAddress currAddress = new InetSocketAddress(
+Integer.parseInt(args[0]));
 
-            //load boards
+//load boards
 
-            currSocket.bind(currAddress); //bind
-            System.out.println("Server is Listening now!");
+currSocket.bind(currAddress); //bind
+System.out.println("Server is Listening now!");
 
-            while (true) {
-                Socket socket = currSocket.accept();
-                System.out.println("Client is connected!");
-                //begin thread
+while (true) {
+Socket socket = currSocket.accept();
+System.out.println("Client is connected!");
+//begin thread
 
-            }
-        } catch (Exception e) {
-            System.out.println("Error Occured!");
-        }
+}
+} catch (Exception e) {
+System.out.println("Error Occured!");
+}
 
-    }
+}
 
 }
